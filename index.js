176b43c9bffbdd6729a5e6fe4e2c128c68754bf0
Tzuyi Yang <a0988426059@gmail.com>
2019-03-13 00:33:05 +0800

    rgbtohex

---
+++ index.js
@@ -0,0 +1,54 @@
+const inputR = document.querySelector('#r')
+const inputG = document.querySelector('#g')
+const inputB = document.querySelector('#b')
+const button = document.querySelector('[data-alpha-pos="convert"]')
+const hex = document.querySelector('#hex')
+const hexColor = document.querySelector('.hexcolor')
+
+// 檢查數字是否輸入錯誤
+button.addEventListener('click', function () {
+  if (inputR.value < 0 || inputR.value > 255 || inputR.value.length === 0) {
+    alert(`請輸入0~255之間的數字`)
+  } else if (inputG.value < 0 || inputG.value > 255 || inputG.value.length === 0) {
+    alert(`請輸入0~255之間的數字`)
+  } else if (inputB.value < 0 || inputB.value > 255 || inputB.value.length === 0) {
+    alert(`請輸入0~255之間的數字`)
+  }
+})
+
+// 轉換hex產生顏色
+button.addEventListener('click', function () {
+  let r = inputR.value
+  let g = inputG.value
+  let b = inputB.value
+  let ans = rgbToHex([r, g, b])
+  console.log(ans)
+  hex.value = ans
+  hexColor.innerHTML = `<div class ="hexcolor" style="background-color:${ans}"></div>`
+})
+
+// rgb轉換hex
+function rgbToHex(rgb) {
+  let hexbe = '#'
+  let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+  for (let i = 0; i < 3; i++) {
+    let hexAns = []
+    let rgbNum = Number(rgb[i])
+    let hexB = ''
+    if (rgbNum > 16) {
+      let hexA = Math.floor(rgbNum / 16)
+      hexB = rgbNum % 16
+      hexAns.push(hex[hexA])
+      hexAns.push(hex[hexB])
+    } else {
+      hexAns.push(hex[rgbNum])
+    }
+
+    if (hexAns.length < 2) {
+      hexAns.reverse(hexAns.push('0'))
+    }
+    hexbe += hexAns.join('')
+  }
+  return hexbe
+}
